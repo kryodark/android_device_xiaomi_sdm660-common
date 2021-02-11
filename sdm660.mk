932e7ffb9b3f20822beb61b6b0f229bcdf538ba9
Markdarkness <mv10marcosmv@gmail.com>
2021-02-11 18:32:39 -0300

    sdm660-common: Drop Lineage AudioFX

--- sdm660.mk
+++ sdm660.mk
@@ -430,5 +430,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     WfdCommon
 
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/sdm660-common/sdm660-common-vendor.mk)
